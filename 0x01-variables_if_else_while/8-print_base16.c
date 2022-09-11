d064496b5b68477b3ffc90c5f9d0144845815ece
Bianca <dev.19bvdmerwe.96@gmail.com>
2022-09-11 13:18:04 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char mine;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	for (mine = 'a'; mine <= 'f'; mine++)
+	{
+		putchar(mine);
+	}
+	putchar('\n');
+	return (0);
+}
