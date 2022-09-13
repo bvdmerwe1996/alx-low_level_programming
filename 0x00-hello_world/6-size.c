d96aaf76a65cf473c5e8a6675c5b7eab1a556b36
Bianca <dev.19bvdmerwe.96@gmail.com>
2022-09-13 04:52:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include
+
+/**
+ * main - put the string in the function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
